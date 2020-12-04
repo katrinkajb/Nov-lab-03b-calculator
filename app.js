bfc2e6a4acd2506af846fd1d2928ccf7cc44d2c4
katrinkajb@gmail.com <katrinkajb@gmail.com>
2020-12-04 15:47:04 -0800

    rounded buttons and inputs

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 // import functions and grab DOM elements
+import {sum} from 'utilis.js';
+
 const addInput1 = document.getElementById("add-input-1");
 const addInput2 = document.getElementById("add-input-2");
 const addButton = document.getElementById("add-button");
 const addResults = document.getElementById("add-results");
 
+const subtractInput1 = document.getElementById("subtract-input-1");
+const subtractInput2 = document.getElementById("subtract-input-2");
+const subtractButton = document.getElementById("subtract-button");
+const subtractResults = document.getElementById("subtract-results");
+
+const multiplyInput1 = document.getElementById("multiply-input-1");
+const multiplyInput2 = document.getElementById("multiply-input-2");
+const multiplyButton = document.getElementById("multiply-button");
+const multiplyResults = document.getElementById("multiply-results");
+
+const divideInput1 = document.getElementById("divide-input-1");
+const divideInput2 = document.getElementById("divide-input-2");
+const divideButton = document.getElementById("divide-button");
+const divideResults = document.getElementById("divide-results");
+
 // initialize state
+
 
 // set event listeners to update state and DOM
 addButton.addEventListener('click', () => {
@@ -15,11 +33,6 @@ addButton.addEventListener('click', () => {
     addResults.textContent = addResult;
 });
 
-const subtractInput1 = document.getElementById("subtract-input-1");
-const subtractInput2 = document.getElementById("subtract-input-2");
-const subtractButton = document.getElementById("subtract-button");
-const subtractResults = document.getElementById("subtract-results");
-
 subtractButton.addEventListener('click', () => {
     const subNumber1 = Number(subtractInput1.value);
     const subNumber2 = Number(subtractInput2.value);
@@ -28,11 +41,6 @@ subtractButton.addEventListener('click', () => {
     subtractResults.textContent = subResult;
 });
 
-const multiplyInput1 = document.getElementById("multiply-input-1");
-const multiplyInput2 = document.getElementById("multiply-input-2");
-const multiplyButton = document.getElementById("multiply-button");
-const multiplyResults = document.getElementById("multiply-results");
-
 multiplyButton.addEventListener('click', () => {
     const multNumber1 = Number(multiplyInput1.value);
     const multNumber2 = Number(multiplyInput2.value);
@@ -40,11 +48,6 @@ multiplyButton.addEventListener('click', () => {
 
     multiplyResults.textContent = multResult;
 });
-
-const divideInput1 = document.getElementById("divide-input-1");
-const divideInput2 = document.getElementById("divide-input-2");
-const divideButton = document.getElementById("divide-button");
-const divideResults = document.getElementById("divide-results");
 
 divideButton.addEventListener('click', () => {
     const divNumber1 = Number(divideInput1.value);
---
+++ utilis.js
@@ -0,0 +1,5 @@
+// declaration - defining input, output, and process
+
+export function sum(thing1, thing2) {
+    return thing1 + thing2;
+}
